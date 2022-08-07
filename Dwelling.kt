ea4d38cfcc9fc86111e119fd9a8211d6864b0401
Haithamsaleh <92247964+Haithamsaleh@users.noreply.github.com>
2022-08-07 14:06:35 +0300

    Update Dwelling.kt
    floorArea &  fun getRoom

--- Dwelling.kt
+++ Dwelling.kt
@@ -1,29 +1,47 @@
+import kotlin.math.PI
 fun main() {
-    val sqaureCabin = SqaureCabin(6)
-    val roundHut = RoundHut(1)
-   val  roundTower= RoundTower(5)
+    val sqaureCabin = SqaureCabin(6,5.0)
+    val roundHut = RoundHut(1,10.0)
+   val  roundTower= RoundTower(50,10.0,10)
+             println("===================================")
+
     with(sqaureCabin){
-                             println("sqaureCabin")
+        println("sqaureCabin")
 
          println("caps : ${caps}")
-     println("Material : ${buildingMaterial} \n ")
+     println("Material : ${buildingMaterial}  ")
      println("room : ${hasRoom()}")
+getRoom()
+          println("area : ${floorArea()}")
+
+          println("===================================")
 
         
     }
         with(roundHut){
                      println("roundHut")
          println("caps : ${caps}")
-     println("Material : ${buildingMaterial} \n ")
+     println("Material : ${buildingMaterial}  ")
      println("room : ${hasRoom()}")
+     getRoom()
+
+               println("area : ${floorArea()}")
+                         println("===================================")
+
+
 
         
     }
          with(roundTower){
-                     println("roundHut")
+                     println("roundTower")
          println("caps : ${caps}")
-     println("Material : ${buildingMaterial} \n ")
+     println("Material : ${buildingMaterial}  ")
      println("room : ${hasRoom()}")
+     getRoom()
+
+       println("area : ${floorArea()}")
+                 println("===================================")
+
 
         
     }
@@ -41,23 +59,60 @@ abstract class Dwelling(private var res: Int) {
     
     fun hasRoom() : Boolean{
         return res <= caps
+        
     }
+    
+    fun getRoom(){
+        if(res <= caps){
+            res++
+            println("U got a room")
+        }else{
+            println("sorry no room")
+        }
+    }
+    
+    
+    
+    abstract fun floorArea() : Double 
+        
+    
+    
+    
    
 
     
     }//end parent class
 
 //cheldren 
-class SqaureCabin (res : Int) : Dwelling(res){
+class SqaureCabin (res : Int, val length : Double ) : Dwelling(res){
     override val buildingMaterial ="wood"
     override val caps = 6
+    
+    override fun floorArea() : Double {
+        return length * length
+    } 
 }
- open class RoundHut (res : Int) : Dwelling(res){
+
+
+
+ open class RoundHut (res : Int, val radius : Double) : Dwelling(res){
      override val buildingMaterial ="straw"
     override val caps = 3
+     
+     override fun floorArea() : Double {
+        return PI * radius * radius
+    } 
 }
+ 
+ 
+ 
  // c of c
-class RoundTower (res : Int) : RoundHut(res){
+class RoundTower (res : Int,radius : Double, val floors: Int = 2) : RoundHut(res, radius){
      override val buildingMaterial ="stone"
-    override val caps = 4
+    override val caps = 4 * floors
+    
+     override fun floorArea() : Double {
+        return super.floorArea() * floors
+    } 
+    
 }
